e8a35642282fe1ac746a7fabdea87119114c2f8c
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-01-04 18:51:42 +0530

    commentDTO validation exception

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class CommentController {
 
     @PostMapping("/post/{postId}/comments")
     public ResponseEntity<CommentDTO> createComment(@PathVariable("postId") Long postId,
-                                                 @RequestBody CommentDTO commentDTO){
+                                                 @Valid @RequestBody CommentDTO commentDTO){
         CommentDTO comment = commentService.createComment(postId, commentDTO);
         return new ResponseEntity<>(comment, HttpStatus.OK);
     }
@@ -34,7 +35,7 @@ public class CommentController {
 
     @GetMapping("/post/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDTO> getCommentById(@PathVariable("postId") Long postId,
-                                                    @PathVariable("commentId") Long commentId){
+                                                     @PathVariable("commentId") Long commentId){
         CommentDTO commentDTO = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDTO, HttpStatus.OK);
     }
@@ -42,14 +43,14 @@ public class CommentController {
     @PutMapping("/post/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDTO> updateComment(@PathVariable("postId") Long postId,
                                                     @PathVariable("commentId") Long commentId,
-                                                    @RequestBody CommentDTO commentDTO){
+                                                    @Valid @RequestBody CommentDTO commentDTO){
         CommentDTO comment = commentService.updateComment(postId, commentId, commentDTO);
         return new ResponseEntity<>(comment, HttpStatus.OK);
     }
 
     @DeleteMapping("/post/{postId}/comments/{commentId}")
     public ResponseEntity<String> updateComment(@PathVariable("postId") Long postId,
-                                                    @PathVariable("commentId") Long commentId){
+                                                @PathVariable("commentId") Long commentId){
         commentService.deleteComment(postId, commentId);
         return new ResponseEntity<>("Comment deleted", HttpStatus.OK);
     }
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDTO.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDTO.java
@@ -2,10 +2,22 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDTO {
     private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Please provide a valid email")
     private String email;
+
+    @NotEmpty(message = "Comment Body should not be empty")
+    @Size(min = 10, message = "Comment should have minimum 10 characters")
     private String body;
 }
