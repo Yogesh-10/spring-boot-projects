8f112c2fd2efee0f7498651d527e4961540d78d6
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-01-09 00:20:23 +0530

    handle accessdenied exception

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.springboot.blog.payload.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -37,17 +39,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    //Handle global exception
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> handleGlobalException(Exception exception,
-                                                         WebRequest webRequest){
-        ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage(),
-                webRequest.getDescription(false));
-
-        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    //validation Exception
+    //validation Exception for Entity(hibernate)
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status,
@@ -63,4 +55,31 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorMessage> handleAccessDeniedException(AccessDeniedException exception,
+                                                                    WebRequest webRequest){
+        ErrorMessage errorDetails = new ErrorMessage(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorMessage> handleAccessDeniedException(AuthenticationException exception,
+                                                                    WebRequest webRequest){
+        ErrorMessage errorDetails = new ErrorMessage(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    //Handle global exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handleGlobalException(Exception exception,
+                                                              WebRequest webRequest){
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/security/JWTAuthenticationEntryPoint.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/security/JWTAuthenticationEntryPoint.java
@@ -1,8 +1,11 @@
 package com.springboot.blog.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +16,19 @@ import java.io.IOException;
 public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
     //This method is called whenever an exception is thrown due to an unauthenticated user trying to access a resource
     //that requires authentication
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Autowired
+    public JWTAuthenticationEntryPoint(HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
         System.out.println("JWTAuthenticationEntryPoint - commence");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        handlerExceptionResolver.resolveException(request, response, null, authException);
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
 }
