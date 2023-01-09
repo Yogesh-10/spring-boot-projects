c38cc0352aba9566626b813a8adcca153b1b7624
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-01-09 19:34:03 +0530

    inital commit

---
+++ springboot-library-app/springboot-library-app/src/main/java/com/springboot/library/SpringbootLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLibraryAppApplication.class, args);
+	}
+
+}
