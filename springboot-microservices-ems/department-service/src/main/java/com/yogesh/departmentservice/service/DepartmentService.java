bbba522e1c3c5e31dcb68025f1e61ad7c329a4c6
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-02-15 15:56:06 +0530

    department microservice

---
+++ springboot-microservices-ems/department-service/src/main/java/com/yogesh/departmentservice/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.yogesh.departmentservice.service;
+
+import com.yogesh.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
