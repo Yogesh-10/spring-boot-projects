5f0742613e6444a10606e12dbd3f1b6de8d33f64
Yogesh-10 <yogiyogesh100800@gmail.com>
2023-02-15 18:22:30 +0530

    service registery eureka client

---
+++ springboot-microservices-ems/service-registry/src/test/java/com/yogesh/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogesh.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
